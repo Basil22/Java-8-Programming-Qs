dd68da17174cec550b24bc594e70d21d47ff7c8a
Basil <basilrasheed24@gmail.com>
2026-03-24 22:18:48 +0530

    Added String Sort examples

---
+++ SortExamples.java
@@ -0,0 +1,59 @@
+package Java8;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortExamples {
+        public static void main(String[] args) {
+
+                // Example 1: Sort by Length of each string
+                List<String> input = Arrays.asList("banana", "apple", "kiwi", "mango");
+
+                List<String> output = input.stream()
+                                .sorted(Comparator.comparing(String::length))
+                                .collect(Collectors.toList());
+
+                System.out.println(output);
+
+                // Example 2: Search by length (Case Insensitive)
+                List<String> input2 = Arrays.asList("Banana", "apple", "Cherry", "mango");
+
+                List<String> output2 = input2.stream()
+                                .sorted(Comparator.comparing(String::toLowerCase))
+                                .collect(Collectors.toList());
+
+                System.out.println(output2);
+
+                // Example 3: Sort by last character of String
+                List<String> input3 = Arrays.asList("banana", "apple", "kiwi", "mango");
+
+                List<String> output3 = input3.stream()
+                                .sorted(Comparator.comparing(a -> a.charAt(a.length() - 1)))
+                                .collect(Collectors.toList());
+
+                System.out.println(output3);
+
+                // Example 4: Sort by number of vowels
+                List<String> input4 = Arrays.asList("apple", "fig", "banana", "kiwi", "orange");
+
+                List<String> output4 = input4.stream()
+                                .sorted(Comparator.comparing((a) -> {
+                                        int vowelCount = 0;
+
+                                        for (int i = 0; i < a.length(); i++) {
+                                                if (a.toLowerCase().charAt(i) == 'a' || a.toLowerCase().charAt(i) == 'e'
+                                                                || a.toLowerCase().charAt(i) == 'i'
+                                                                || a.charAt(i) == 'o' || a
+                                                                                .toLowerCase().charAt(i) == 'u') {
+                                                        vowelCount++;
+                                                }
+                                        }
+                                        return Integer.max(vowelCount, a.length());
+                                }))
+                                .collect(Collectors.toList());
+
+                System.out.println(output4);
+        }
+}
